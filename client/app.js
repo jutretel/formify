9d373bc3a8a62f2e7441ffc1325857c46be840ee
VictorGeorge <victor.georgemattos@gmail.com>
2017-11-22 00:59:14 -0200

    Criação e verificação de form para gerar certificado

--- client/app.js
+++ client/app.js
@@ -8,6 +8,7 @@ angular.module('formify',['ngRoute', 'ngCookies'])
     .when('/event',{templateUrl:'templates/event-list.html', controller:'eventCtrl'})
     .when('/event/new',{templateUrl:'templates/new-event.html', controller:'newEventCtrl'})
     .when('/search',{templateUrl:'templates/search.html', controller:'searchCtrl'})
+    .when('/certificate',{templateUrl:'templates/certificate.html', controller:'certificateCtrl'})
     .when('/',{templateUrl:'templates/event-list.html', controller:'eventListCtrl'})
 
     .when('/person/list',{templateUrl:'templates/person-list.html', controller:'personListCtrl'})
